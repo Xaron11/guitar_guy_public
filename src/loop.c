d6788500fffc57dc04d3d6b06feb10c1815f349d
mateuszborucki <mateuszborucki@icloud.com>
2025-05-07 21:12:49 +0200

    feat: improve score and combo display in game loop; add song progress bar

--- src/loop.c
+++ src/loop.c
@@ -1,6 +1,10 @@
-#include "loop.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
 #include "draw.h"
 #include "game.h"
+#include "loop.h"
+#include "map.h"
 #include <raylib.h>
 
 extern int score;
@@ -18,12 +22,39 @@ void GameLoop(void) {
     ClearBackground(BLACK);
 
     DrawColumnLines();
-    DrawText(TextFormat("Score: %d", score), 10, 10, 20, WHITE);
-    DrawText(TextFormat("Combo: %d", GetCombo()), 10, 30, 20, YELLOW);
-    DrawText(TextFormat("Multiplier: x%d", GetMultiplier()), 10, 50, 20,
-             ORANGE);
-    DrawText(songTitle, 10, 80, 20, WHITE);
-    DrawText(songArtist, 10, 100, 20, GRAY);
+    DrawText("SCORE", GetScreenWidth() / 2 - MeasureText("SCORE", 24) / 2, 40,
+             24, GRAY);
+    DrawText(TextFormat("%d", score),
+             GetScreenWidth() / 2 -
+                 MeasureText(TextFormat("%d", score), 64) / 2,
+             70, 64, YELLOW);
+
+    if (GetCombo() > 0) {
+      DrawText(TextFormat("COMBO: %d", GetCombo()),
+               GetScreenWidth() / 2 -
+                   MeasureText(TextFormat("COMBO: %d", GetCombo()), 36) / 2,
+               140, 36, ORANGE);
+    }
+    if (GetMultiplier() > 1) {
+      DrawText(TextFormat("x%d", GetMultiplier()),
+               GetScreenWidth() / 2 -
+                   MeasureText(TextFormat("x%d", GetMultiplier()), 48) / 2,
+               180, 48, RED);
+    }
+
+    DrawText(songTitle, 20, 40, 20, LIGHTGRAY);
+    DrawText(songArtist, 20, 65, 18, DARKGRAY);
+
+    int notesHit = 0;
+    for (int i = 0; i < noteCount; i++) {
+      if (!notes[i].active)
+        notesHit++;
+    }
+    float progress =
+        (noteCount > 0) ? ((float)notesHit / (float)noteCount) : 0.0f;
+    Rectangle barBounds = {40.0f, 8.0f, (float)(GetScreenWidth() - 80), 24.0f};
+    GuiProgressBar(barBounds, "", "", &progress, 0.0f, 1.0f);
+
     DrawButtons();
     DrawNotes();
 
